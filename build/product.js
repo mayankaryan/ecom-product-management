5760409ffd1a3b33099dadac77969131c1230f01
Mayank Aryan <Mayank.Aryan@godigit.com>
2024-08-08 11:15:27 +0530

    (added): crate product model js file

---
+++ build/product.js
@@ -0,0 +1,9 @@
+"use strict";
+class Product {
+    constructor(name, id, rating, review) {
+        this.name = name;
+        this.id = id;
+        this.rating = rating;
+        this.review = review;
+    }
+}
