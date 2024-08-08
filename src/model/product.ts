fd73d77f59cd4e1d92ff64e434fbc37f839f78dd
Mayank Aryan <Mayank.Aryan@godigit.com>
2024-08-08 11:10:07 +0530

    (added): product model

---
+++ src/model/product.ts
@@ -0,0 +1,13 @@
+class Product {
+    name: string;
+    id: number;
+    rating: number;
+    review: string;
+
+    constructor(name:string, id:number, rating:number, review: string){
+        this.name = name;
+        this.id = id;
+        this.rating = rating;
+        this.review = review;
+    }
+}
